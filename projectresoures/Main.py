4473c640a0f744c8f2ebe1d811bc1f143ccdafdd
Ashley <61482604+Narnar-the-Rat-Girl@users.noreply.github.com>
2025-11-25 08:15:19 -0700

    update Main.py

--- projectresoures/Main.py
+++ projectresoures/Main.py
@@ -1,15 +1,32 @@
 
-class doctor:
-   def __str__(self):
-       pass
+
+
+
+
+
+# Ashley
+class Doctor:
+   def __str__(self, doc_id='null', specialization='null', working_time='null',qualification='null', room_num='null'):
+       self.doc_id = doc_id
+       self.specialization = specialization
+       self.working_time = working_time
+       self.qualification = qualification
+       self.room_num = room_num
        
 
 
-
-class doctormanager:
+#ashley
+class DoctorManager:
     pass
 
+class Patient:
+    pass
 
+class PatientManager:
+    pass
+
+class Management:
+    pass
 
 #class 1 and 2 ashely
 #class 3 
