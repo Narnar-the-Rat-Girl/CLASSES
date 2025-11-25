9828c98c7900e0bef9ed75658ee08d20f23f3d5c
Ashley <61482604+Narnar-the-Rat-Girl@users.noreply.github.com>
2025-11-25 09:04:02 -0700

    update Main.py

--- projectresoures/Main.py
+++ projectresoures/Main.py
@@ -1,55 +1,83 @@
-
-
-
-
-
-
 # Ashley
 class Doctor:
-    def __init__(self, doc_id='', name ='', specialization='', working_time='',qualification='', room_num=''):
-       self.doc_id = doc_id
-       self.name = name
-       self.specialization = specialization
-       self.working_time = working_time
-       self.qualification = qualification
-       self.room_num = room_num
-       
+    def __init__(
+        self,
+        doc_id="",
+        name="",
+        specialization="",
+        working_time="",
+        qualification="",
+        room_num="",
+    ):
+        self.doc_id = doc_id
+        self.name = name
+        self.specialization = specialization
+        self.working_time = working_time
+        self.qualification = qualification
+        self.room_num = room_num
+
     def get_doc_id(self):
-        return(self.doc_id)
-    
+        return self.doc_id
+
     def get_name(self):
-        return(self.name)
-    
+        return self.name
+
     def get_specialization(self):
-        return(self.specialization)
-    
+        return self.specialization
+
     def get_working_time(self):
-        return(self.working_time)
-   
+        return self.working_time
+
     def get_qualification(self):
-        return(self.qualification)
-    
+        return self.qualification
+
     def get_room_num(self):
-        return(self.room_num)
-        
+        return self.room_num
+
+    def set_doc_id(self, new_id):
+        self.doc_id = new_id
+
+    def set_name(self, new_name):
+        self.name= new_name
+
+    def set_specialization(self, new_specialization):
+        self.doc_specialization = new_specialization
+
+    def set_working_time(self, new_working_time):
+        self.working_time = new_working_time
+
+    def set_qualification(self, new_qualification):
+        self.doc_qualification = new_qualification
+
+    def set_room_num(self, new_room_num):
+        self.room_num = new_room_num
+    
+    
+    #TODO __str__()
+    def __str__():
+        pass
 
 
-#ashley
+# ashley
 class DoctorManager:
     pass
+
 
 class Patient:
     pass
 
+
 class PatientManager:
     pass
+
 
 class Management:
     pass
 
-#class 1 and 2 ashely
-#class 3 
-#class 4
+
+# class 1 and 2 ashely
+# class 3
+# class 4
 
 
 if __name__ == "__main__":
