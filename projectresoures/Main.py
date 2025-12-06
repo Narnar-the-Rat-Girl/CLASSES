2742e5af12ea2748c3cb22876e0d5e23e393252d
chloeallison2007 <chloeallison2007@gmail.com>
2025-12-05 20:55:36 -0700

    Update Main.py
    patient class done

--- projectresoures/Main.py
+++ projectresoures/Main.py
@@ -64,41 +64,46 @@ class DoctorManager:
 
 #chloe
 class Patient:
+    #initializes the properties for each patient object
     def __innit__(self, name="", age="", pid="", disease="", gender=""):
         self.name = name
         self.age = age
         self.pid = pid
         self.disease = disease
         self.gender = gender
-
+    #patient name
     def get_name(self):
         return self.name
     def set_name(self, new_name):
         self.name = new_name
-
+        
+    #patient age
     def get_age(self):
         return self.age
     def set_age(self, new_age):
         self.age = new_age
 
+    #patient ID
     def get_pid(self):
         return self.pid
     def set_pid(self, new_pid):
         self.pid = new_pid
 
+    #patient's disease
     def get_disease(self):
         return self.disease
     def set_disease(self, new_disease):
         self.disease = new_disease
 
+    #patient's gender
     def get_gender(self):
         return self.gender
     def set_gender(self, new_gender):
         self.gender = new_gender
 
-    #
-    def __str__():
-        pass
+    #returns the patient objects as a formatted string
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}, Patient ID: {self.pid}, Disease: {self.disease}, Gender:{self.gender}"
     
 
 #chloe
